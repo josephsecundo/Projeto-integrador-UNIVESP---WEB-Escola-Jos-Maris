614a7eb56f754dfaf2389486b0e19aaa8ddbc09d
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2025-04-21 20:27:19 -0300

    Angular ultima atualização

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -3,9 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+    standalone: false
 })
 export class LoginComponent {
   login: string = '';
